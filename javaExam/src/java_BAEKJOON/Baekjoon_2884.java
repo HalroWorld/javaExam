1653a84f023e2a98d14b41feb91cbf99b4e41d71
bong <halro@z.z>
2023-01-15 21:22:40 +0900

    1월 15일 자바 연습문제(백준 조건문)

---
+++ javaExam/src/java_BAEKJOON/Baekjoon_2884.java
@@ -0,0 +1,28 @@
+package java_BAEKJOON;
+import java.util.Scanner;
+public class Baekjoon_2884 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		int H = scan.nextInt();
+		int M = scan.nextInt();
+			
+		if(M<45 && H==0) {
+			H = 23;
+			M = 60-(45-M);
+			System.out.print(H + " ");	
+			System.out.print(M);
+		}else if(M<45 && H!=0) {
+			H-=1;
+			M = 60-(45-M);
+			System.out.print(H + " ");	
+			System.out.print(M);
+		} else {
+			M -= 45;
+			System.out.print(H + " ");	
+			System.out.print(M);
+		}
+	}
+
+}
